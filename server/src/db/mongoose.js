4242d13981359fc7306f87136baaaa83f320f2f9
PakDragoon <pagan1581998@gmail.com>
2022-10-14 10:36:12 +0500

    rollback one

--- server/src/db/mongoose.js
+++ server/src/db/mongoose.js
@@ -1,16 +1,16 @@
 const mongoose = require("mongoose")
 
 //Development
-// mongoose
-//   .connect("mongodb://127.0.0.1:27017/draggable-form", {})
-//   .then(() => console.log("Connected to database "))
-//   .catch((err) => console.error(`Error connecting to the database. \n${err}`))
-
-//Production
-const password = `gbWw4LX3u9eUtkUL`
-const url = `mongodb+srv://pakdragoon:${password}@cluster0.o6jzh1o.mongodb.net/draggable-form-v1?retryWrites=true&w=majority&ssl=true`
-
 mongoose
-  .connect(url, {})
+  .connect("mongodb://127.0.0.1:27017/draggable-form", {})
   .then(() => console.log("Connected to database "))
   .catch((err) => console.error(`Error connecting to the database. \n${err}`))
+
+//Production
+// const password = ``
+// const url = ``
+
+// mongoose
+//   .connect(url, {})
+//   .then(() => console.log("Connected to database "))
+//   .catch((err) => console.error(`Error connecting to the database. \n${err}`))
