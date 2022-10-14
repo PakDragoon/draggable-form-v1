092638e6efee6cb90cebcd39823536f425caedcb
PakDragoon <pagan1581998@gmail.com>
2022-10-14 10:33:00 +0500

    mongo cloud connection

--- server/src/db/mongoose.js
+++ server/src/db/mongoose.js
@@ -1,16 +1,16 @@
 const mongoose = require("mongoose")
 
 //Development
-mongoose
-  .connect("mongodb://127.0.0.1:27017/draggable-form", {})
-  .then(() => console.log("Connected to database "))
-  .catch((err) => console.error(`Error connecting to the database. \n${err}`))
-
-//Production
-// const password = `tdfxd0aog3QYV94x`
-// const url = `mongodb+srv://user1:${password}@cluster0.ld4pg.mongodb.net/async-now?retryWrites=true&w=majority&ssl=true`
-
 // mongoose
-//   .connect(url, {})
+//   .connect("mongodb://127.0.0.1:27017/draggable-form", {})
 //   .then(() => console.log("Connected to database "))
 //   .catch((err) => console.error(`Error connecting to the database. \n${err}`))
+
+//Production
+const password = `gbWw4LX3u9eUtkUL`
+const url = `mongodb+srv://pakdragoon:${password}@cluster0.o6jzh1o.mongodb.net/draggable-form-v1?retryWrites=true&w=majority&ssl=true`
+
+mongoose
+  .connect(url, {})
+  .then(() => console.log("Connected to database "))
+  .catch((err) => console.error(`Error connecting to the database. \n${err}`))
